42e0f436f2346b14ce0099ae375dacf04d656b4e
Luca Faggianelli <luca.faggianelli@datalogic.com>
2016-05-03 11:25:28 +0200

    Fix #3 can edit preferences
    Preferences are stored in user home path checking environmental var:
     - UNIX: $HOME
     - WIN: $USERPROFILE
    
    The .json are in the $HOME/sibilla folder
    
    Removed src/services.js

--- src/preferences.js
+++ src/preferences.js
@@ -5,8 +5,65 @@ angular.module('preferences', [])
   $scope.prefs = Preferences.get();
 
   $scope.saveSettings = function() {
-    console.log($scope.prefs);
-    //Preferences.save($scope.prefs);
+    Preferences.save($scope.prefs);
   };
+
+}])
+
+.factory('Preferences', ['Flash', function(Flash) {
+  // Prefences store
+  var prefs = {};
+  var prefPath = path.join(getUserHome(), 'sibilla');
+  var prefFile = path.join(prefPath, 'preferences.json');
+
+  function getUserHome() {
+    return process.env[(process.platform == 'win32') ? 'USERPROFILE' : 'HOME'];
+  }
+
+  function initPreferences() {
+    const prefsDefault = JSON.parse(
+        fs.readFileSync('preferences.default.json', 'utf8'));
+
+    try {
+      fs.mkdir(prefPath, function() {});
+      console.log('User pref file', prefFile);
+
+      var prefsUser = JSON.parse(fs.readFileSync(prefFile, 'utf8'));
+    } catch (err) {
+      console.warn("Can't read user preferences", err);
+    }
+
+    prefs = angular.extend(prefsDefault, prefsUser);
+  }
+
+  initPreferences();
+
+  /*
+   * Service interface
+   */
+  var service = {};
+
+  service.get = function(name) {
+    if (!name)
+      return prefs;
+    else
+      return prefs[name];
+  };
+
+  service.save = function(new_prefs) {
+    console.log('Saving pref', new_prefs);
+
+    fs.mkdir(prefPath, function(err) {
+      if (err && err.code != 'EEXIST') {
+        var msg = 'Settings not saved. Can\'t create folder preferences folder ' + prefPath;
+        Flash.create('error', msg);
+        console.warn(msg)
+        return;
+      }
+      fs.writeFileSync(prefFile, JSON.stringify(new_prefs, null, 2), 'utf8');
+    });
+  };
+
+  return service;
 }])
 
