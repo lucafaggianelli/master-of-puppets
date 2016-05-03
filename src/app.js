d6215a00368277937f25fa6d3c1be3cfb5168207
Luca Faggianelli <luca.faggianelli@gmail.com>
2016-05-03 22:10:50 +0200

    Fix drives mountpoint mapping
    Files couldn't be opened and the filepicker was broken too.
    
    Using a mapping between drive's _id and drives local mountpoint:
     {
       '12314ab2134532b87fe': '/mnt/mydrive'
     }

--- src/app.js
+++ src/app.js
@@ -40,7 +40,9 @@ angular.module('sibilla', [
   $scope.drives_available = Drive.query(function() {
     $scope.initDrives();
   });
-  $scope.drives_local = {};
+
+  // Mapping of drive _id to drive mountpoint
+  $scope.drivesMountpoint = {};
 
   /* Document actions */
   $scope.editDocument = function(doc) {
@@ -66,19 +68,9 @@ angular.module('sibilla', [
       this.docsForm.files.unshift(this.docsForm.newFile);
     }
 
-    /*
-    this.docsForm.categories =
-        this.docsForm.categories.map(function(x){return x.id});
-    this.docsForm.tags =
-        this.docsForm.tags.map(function(x){return x.id});
-    if (this.docsForm.drive)
-      this.docsForm.drive = this.docsForm.drive.id;*/
-
     if (this.docsForm.id) {
-      console.log("Update doc");
       this.docsForm.$update();
     } else {
-      console.log("Create doc");
       this.docsForm.$save();
     }
   };
@@ -91,17 +83,12 @@ angular.module('sibilla', [
   $scope.openFilePicker = function() {
     var drive_id = $scope.docsForm.drive;
 
-    console.log('local drives', $scope.drives_local);
-    console.log('remote drives', $scope.drives_available);
-
-    for (var i in $scope.drives_available) {
-      if ($scope.drives_available[i].id == drive_id) {
-        var root = $scope.drives_local[$scope.drives_available[i].drive_id];
-        $scope.$broadcast('filepicker:setRoot', root);
-        return;
-      }
+    if (drive_id in $scope.drivesMountpoint) {
+      var root = $scope.drivesMountpoint[drive_id];
+      $scope.$broadcast('filepicker:setRoot', root);
+    } else {
+      console.warn("Can't find drive", drive_id);
     }
-    console.warn("Can't find drive", drive_id);
   }
 
   $scope.openFile = function(doc, revision) {
@@ -112,12 +99,16 @@ angular.module('sibilla', [
       return;
     }
 
-    var drive = Preferences.get('drives')[doc.drive] || '/';
-    var filename = doc.files[rev];
-    var abs_path = path.join(drive, filename);
-
-    console.log('opening file', abs_path);
-    shell.openItem(abs_path);
+    if (doc.drive in $scope.drivesMountpoint) {
+      var drive = $scope.drivesMountpoint[doc.drive];
+      var filename = doc.files[rev];
+      var abs_path = path.join(drive, filename);
+      console.log('opening file', abs_path);
+      shell.openItem(abs_path);
+    } else {
+      console.warn("Drive not found", doc.drive);
+      return;
+    }
   };
 
   $scope.selectCategory = function(cat) {
@@ -131,20 +122,17 @@ angular.module('sibilla', [
         return;
       }
 
-      console.log('Drives from server', $scope.drives_available);
       njds.drivesDetail(drives, function(err, data) {
-        for (var i in data) {
-          $scope.drives_local[data[i].drive] = data[i].mountpoint;
-        }
-
-        /*
         for (var i in $scope.drives_available) {
           var drive_id = $scope.drives_available[i].drive_id;
-          if (drive_id in $scope.drives_local) {
-            $scope.drives_available[i].mount = $scope.drives_local[drive_id];
+          // Search local mountpoint
+          for (var j in data) {
+            if (drive_id == data[j].drive) {
+              $scope.drivesMountpoint[$scope.drives_available[i].id] = data[j].mountpoint;
+            }
           }
         }
-        console.log('Found local drives', $scope.drives_local);*/
+        console.log('Found local drives', $scope.drivesMountpoint);
       });
     });
   }
